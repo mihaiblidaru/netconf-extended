40e47c860b95747413f6efb79c0816cbdd491540
Mihai Blidaru <mihaiblidaru14@gmail.com>
2020-04-17 20:51:36 +0200

    create wheel

--- buildinstall.sh
+++ buildinstall.sh
@@ -7,7 +7,6 @@ if [ "$#" -eq  "1" ]; then
         fi
 fi
         
-
 python3 -m pip install -r requirements.txt $install_as_user
-python3 setup.py build
+python3 setup.py sdist bdist_wheel
 python3 setup.py install $install_as_user
